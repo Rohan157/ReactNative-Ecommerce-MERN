75c8d93204b0fd040dd96ef25a808fa4ae1c2ce5
Rohan157 <73091151+Rohan157@users.noreply.github.com>
2021-05-21 18:05:02 +0500

    Categories fields added

--- backend/app.js
+++ backend/app.js
@@ -3,32 +3,44 @@ const app = express();
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv/config");
+
+app.use(cors());
+app.options("*", cors());
 
 //middleware
-
 app.use(bodyParser.json());
 app.use(morgan("tiny"));
 
-require("dotenv/config");
+//Routes
+const categoriesRoutes = require("./routes/categories");
+const productsRoutes = require("./routes/products");
+const usersRoutes = require("./routes/users");
+const ordersRoutes = require("./routes/orders");
 
 const api = process.env.API_URL;
-const productsRouter = require("./routers/products");
 
-app.use(`${api}/products`, productsRouter);
+app.use(`${api}/categories`, categoriesRoutes);
+app.use(`${api}/products`, productsRoutes);
+app.use(`${api}/users`, usersRoutes);
+app.use(`${api}/orders`, ordersRoutes);
 
+//Database
 mongoose
   .connect(process.env.CONNECTION_STRING, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    dbName: "mern-eshop",
+    dbName: "eshop-database",
   })
   .then(() => {
-    console.log("database connected");
+    console.log("Database Connection is ready...");
   })
   .catch((err) => {
     console.log(err);
   });
 
+//Server
 app.listen(3000, () => {
-  console.log("server running");
+  console.log("server is running http://localhost:3000");
 });
---
+++ backend/routes/users.js
@@ -0,0 +1,34 @@
+const { User } = require("../models/user");
+const express = require("express");
+const router = express.Router();
+
+router.get(`/`, async (req, res) => {
+  const userList = await User.find();
+
+  if (!userList) {
+    res.status(500).json({ success: false });
+  }
+  res.send(userList);
+});
+
+router.post("/", async (req, res) => {
+  let user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    color: req.body.color,
+    passwordHash: req.body.passwordHash,
+    phone: req.body.phone,
+    isAdmin: req.body.isAdmin,
+    apartment: req.body.apartment,
+    zip: req.body.zip,
+    city: req.body.city,
+    country: req.body.country,
+  });
+  user = await user.save();
+
+  if (!user) return res.status(400).send("the user cannot be created!");
+
+  res.send(user);
+});
+
+module.exports = router;
